ba075189b6a66701f082c87a9781a341295c44df
Maxim Matsiushonak <matmak122@gmail.com>
2021-10-25 13:02:26 +0300

    Fix movie cards
    - If there is no image, the corresponding message is displayed
    - If there is no plot, the corresponding message is displayed
    - Added more adaptability

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import image from '../images/no_image.jpg';
+import { selectStatus } from '../redux/moviesSlice';
 
-const MovieCard = ({ Plot, Poster, Title, imdbID }) => (
-  <Card component={Link} to={`/${imdbID}`} sx={{ maxWidth: 345 }}>
-    <CardActionArea l>
-      <CardMedia component="img" height="140" image={Poster} alt={Title} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {Title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {Plot}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
-);
+const MovieCard = ({ Plot, Poster, Title, imdbID }) => {
+  const status = useSelector(selectStatus);
+  return (
+    <Card
+      to={`/${imdbID}`}
+      sx={{ textDecoration: 'none' }}
+      component={status === 'idle' ? Link : 'span'}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          sx={{ height: { xs: 300, md: 400 } }}
+          image={Poster !== 'N/A' ? Poster : image}
+          alt={Title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {Title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {Plot !== 'N/A' ? Plot : 'The plot is missing'}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 MovieCard.propTypes = {
   Plot: PropTypes.string,
