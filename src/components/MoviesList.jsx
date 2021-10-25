9df616ec47234eed92912077d76bef965934692a
Maxim Matsiushonak <matmak122@gmail.com>
2021-10-25 13:10:30 +0300

    Add swipe carousel

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Box, Button, Stack } from '@mui/material';
+import ReactSwipe from 'react-swipe';
 import MovieCard from './MovieCard';
 import { selectMovies } from '../redux/moviesSlice';
 
@@ -8,15 +10,39 @@ const MoviesList = () => {
   if (movies.length === 0) {
     return <h3>It looks empty...</h3>;
   }
-  return movies.map((movie) => (
-    <MovieCard
-      key={movie.imdbID}
-      Plot={movie.Plot}
-      Poster={movie.Poster}
-      Title={movie.Title}
-      imdbID={movie.imdbID}
-    />
-  ));
+
+  let reactSwipeEl;
+
+  return (
+    <Stack spacing={2} width="100%">
+      <Box
+        maxWidth={{ xs: '100%', md: '100%' }}
+        padding={1}
+        alignContent="center"
+        component={ReactSwipe}
+        swipeOptions={{ continuous: true }}
+        ref={(el) => {
+          reactSwipeEl = el;
+          return reactSwipeEl;
+        }}
+      >
+        {movies.map((movie) => (
+          <Box component="div" key={movie.imdbID}>
+            <MovieCard
+              Plot={movie.Plot}
+              Poster={movie.Poster}
+              Title={movie.Title}
+              imdbID={movie.imdbID}
+            />
+          </Box>
+        ))}
+      </Box>
+      <Stack direction="row" justifyContent="center" spacing={2}>
+        <Button onClick={() => reactSwipeEl.prev()}>Previous</Button>
+        <Button onClick={() => reactSwipeEl.next()}>Next</Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default MoviesList;
